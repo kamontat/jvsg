b044fa0dfb361aed8ff272ea60ff5b55ecd0f89b
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-09-21 13:39:10 +0700

    chore: upgrade bash and go client image

--- scripts/common/base-settings.sh
+++ scripts/common/base-settings.sh
@@ -6,8 +6,8 @@ export GCP_PROJECT_NAME="perfect-crawler-363108"
 export DEFAULT_ZONE="asia-southeast1-b"
 
 export SERVER_INTERNAL_IP="10.148.0.8"
-export BASHCLIENT_CONTAINER_IMAGE="ghcr.io/kamontat/jvsg-bashclient:sha-28cb04c"
-export GOCLIENT_CONTAINER_IMAGE="ghcr.io/kamontat/jvsg-goclient:sha-5fe211d"
+export BASHCLIENT_CONTAINER_IMAGE="ghcr.io/kamontat/jvsg-bashclient:sha-dfe8f26"
+export GOCLIENT_CONTAINER_IMAGE="ghcr.io/kamontat/jvsg-goclient:sha-dfe8f26"
 export JAVACLIENT_CONTAINER_IMAGE="ghcr.io/kamontat/jvsg-javaclient:sha-ab88f39"
 
 export BASHCLIENT_CONTAINER_RESTART_POLICY="on-failure"
