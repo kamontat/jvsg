5e5c85b0034f8606447da32efb259cca985d6e77
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-09-21 12:57:42 +0700

    chore: add more metrics on server side

--- server/metrics.go
+++ server/metrics.go
@@ -13,8 +13,12 @@ var (
 		Help: "How many request hit server",
 	}, []string{"path", "status", "debug"})
 
-	requestDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "server_request_ms",
+	requestDuration = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "server_request_ms",
+		Help: "How long it takes to process request",
+	}, []string{"path", "status", "debug"})
+	requestDurationBucket = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "server_request_bucket_ms",
 		Help:    "How long it takes to process request",
 		Buckets: []float64{1, 3, 5, 10, 20, 50, 100, 200, 500, 1000},
 	}, []string{"path", "status", "debug"})
@@ -26,5 +30,7 @@ func NewRequestMetric(path, status, debug string) {
 
 func RequestDurationMetric(path, status, debug string, start time.Time) {
 	var duration = float64(time.Since(start).Milliseconds())
-	requestDuration.WithLabelValues(path, status, debug).Observe(duration)
+
+	requestDuration.WithLabelValues(path, status, debug).Set(duration)
+	requestDurationBucket.WithLabelValues(path, status, debug).Observe(duration)
 }
